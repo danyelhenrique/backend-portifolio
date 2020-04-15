3bc4e6f9af1d317821689d26ee19ed0d4ea35c32
danyelhenrique <danyelhenriquefidel@gmail.com>
2020-04-15 00:09:21 -0300

    chore: att types of typescript i project model

--- src/api/models/Project.ts
+++ src/api/models/Project.ts
@@ -6,20 +6,59 @@ export interface IProject extends Document {
   background_url: String;
   deploy_url: String;
   github_url: String;
-  tag: String[];
+  tag: ITag[];
 }
 
-const Project = new Schema({
-  title: String,
-  description: String,
-  background_url: String,
-  deploy_url: String,
-  github_url: String,
-  tag: [
-    {
-      type: String
-    }
-  ]
-});
+export interface ITag extends Document {
+  name: String;
+  _id: Schema.Types.ObjectId;
+}
 
-export default model<IProject>("Project", Project);
+export type TProject = Document & {
+  title: String;
+  description: String;
+  background_url: String;
+  deploy_url: String;
+  github_url: String;
+  tag: ITag[];
+};
+
+export type TTag = ITag & Document;
+
+export type TProjectWithTag = Document & TProject & TTag[];
+
+const TagSchema = new Schema(
+  {
+    name: { type: String }
+  },
+  { timestamps: true }
+);
+
+const ProjectSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    background_url: {
+      type: String,
+      required: true
+    },
+    deploy_url: String,
+    github_url: String,
+    tag: [TagSchema]
+  },
+  { timestamps: true }
+);
+
+const Project = model<TProjectWithTag>("Project", ProjectSchema);
+
+const Tag = model<TTag>("Tag", TagSchema);
+
+export default Project;
+
+export { Tag, Project };
