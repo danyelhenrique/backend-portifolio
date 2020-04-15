960247f7df86fd6fa7842c611e14727184a1edef
danyelhenrique <danyelhenriquefidel@gmail.com>
2020-04-15 00:06:38 -0300

    chore: add default avatar to user

--- src/api/models/User.ts
+++ src/api/models/User.ts
@@ -9,14 +9,29 @@ export interface IUser {
 
 export interface IUserSchema extends Document, IUser {}
 
+const defaultAvatar =
+  "https://m.media-amazon.com/images/M/MV5BZDY2Nzk5MGMtMTVjZS00MGUxLTk1YjQtNTFiYWJiYjc0YjYzXkEyXkFqcGdeQXVyNjUxMjc1OTM@._V1_UY268_CR116,0,182,268_AL_.jpg";
+
 const UserSchema: Schema = new Schema(
   {
-    name: String,
-    avatar_url: String,
-    email: String,
+    name: {
+      type: String,
+      required: true
+    },
+    avatar_url: {
+      type: String,
+      required: true,
+      default: defaultAvatar
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
     password: {
       type: String,
-      select: false
+      select: false,
+      required: true
     }
   },
   { timestamps: true }
